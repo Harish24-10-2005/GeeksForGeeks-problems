f57b387a10fbda8accb64e2daaa5cbdaa9317e36
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-13 18:49:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -4,24 +4,28 @@ class Solution {
     // be performed in a meeting room.
     int maxMeetings(vector<int>& start, vector<int>& end) {
         // Your code here
-        vector<pair<int,int>>m;
-        for(int i=0;i<end.size();i++)
+        int n = start.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<n;i++)
         {
-            m.push_back({end[i],start[i]});
+            pq.push({end[i] ,start[i]});
         }
-            
-        sort(m.begin(),m.end());
         
-        int met = 0;
+        int ans = 0;
         int pre = -1;
-        for(auto a:m)
+        while(!pq.empty())
         {
-            if(a.second > pre)
+            auto [e,s] = pq.top();
+            pq.pop();
+            if(s>pre)
             {
-                met++;
-                pre = a.first;
+                ans++;
+                // cout<<s<<" ->"<<e<<endl;
+                pre = e;
             }
+            // cout<<s<<" ->"<<e<<endl;
+
         }
-        return met;
+        return ans;
     }
 };
