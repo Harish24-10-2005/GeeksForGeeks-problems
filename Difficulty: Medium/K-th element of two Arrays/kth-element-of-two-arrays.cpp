0ea477ea34b35e1a0dacb235f2ce9e50a504e44f
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-21 22:10:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,40 +1,39 @@
 class Solution {
   public:
-    int kthElement(vector<int>& a, vector<int>& b, int k) {
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
         // code here
         int n = a.size();
         int m = b.size();
-        
-        if(m<n) return kthElement(b,a,k);
-    
-        int nm = n+m;
-        int low = max(0,k-m);
-        int high = min(n,k);
-        
-        while(low<=high)
+        if(m<n) kthElement(b,a,k);
+        int l = max(0, k - m);
+        int r = min(k, n);
+        while(l<=r)
         {
-            int m1 = (low + high)/2;
+            int m1 = l+((r-l)/2);
             int m2 = k-m1;
             
-            int l1 = INT_MIN,l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
             
-            if(m1-1 >= 0) l1 = a[m1-1];
-            if(m2-1 >= 0) l2 = b[m2 -1];
+            if(m1-1>=0) l1 = a[m1-1];
+            if(m2-1>=0) l2 = b[m2-1];
             
             if(m1 < n) r1 = a[m1];
             if(m2 < m) r2 = b[m2];
             
-            if(l1<=r2 && l2<=r1)
+            if(l1<=r2 && l2 <=r1)
             {
                 return max(l1,l2);
             }
             if(l1 > r2)
             {
-                high = m1 -1;
+                r = m1-1;
             }
             else
             {
-                low = m1 + 1;
+                l = m1+1;
             }
         }
         return -1;
