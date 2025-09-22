886fd9d125e017b0f4246f552ce66bdcac4cbd92
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-09-22 11:54:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,4 +1,20 @@
 class Solution {
+  private:
+    // int rec(int i,int w,vector<int>&wt,vector<int>&val)
+    // {
+    //     if(w == 0) return 0;
+    //     if( i == val.size())
+    //     {
+    //         return 0;
+    //     }
+    //     int t = 0;
+    //     if(w-wt[i] >= 0)
+    //     {
+    //         t = val[i] + rec(i+1,w-wt[i],wt,val);
+    //     }
+    //     int nt = rec(i+1,w,wt,val);
+    //     return max(t,nt);
+    // }
   public:
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
@@ -6,9 +22,9 @@ class Solution {
         vector<int>dp(W+1,0);
         for(int i=0;i<n;i++)
         {
-            for(int j=W;j>=wt[i];j--)
+            for(int w = W;w>=wt[i];w--)
             {
-                dp[j] = max(dp[j],val[i]+dp[j-wt[i]]);
+                dp[w] = max(dp[w],val[i]+dp[w - wt[i]]);
             }
         }
         return dp[W];
