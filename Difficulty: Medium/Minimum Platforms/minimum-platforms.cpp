305b7e8b06d43f1d3185210391c0e6c6ea58715e
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-11-29 10:15:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -1,35 +1,26 @@
 class Solution {
   public:
-    // Function to find the minimum number of platforms required at the
-    // railway station such that no train waits.
-    int findPlatform(vector<int>& arr, vector<int>& dep) {
-        // Your code here
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        for(int i=0;i<arr.size();i++)
-        {
-            pq.push({arr[i],dep[i]});
-        }
-        vector<int>pred;
-        pred.push_back(-1);
-        while(!pq.empty())
-        {
-            auto a= pq.top();
-            pq.pop();
-            
-            int de = a.second;
-            int ar = a.first;
-            bool fit = false;
-            for(int i=0;i<pred.size();i++)
-            {
-                if(ar > pred[i])
-                {
-                    fit = true;
-                    pred[i] = de;
-                    break;
-                }
-            }
-            if(!fit) pred.push_back(de);
-        }
-        return pred.size();
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        // code here
+       int ans = 1;
+       sort(arr.begin(),arr.end());
+       sort(dep.begin(),dep.end());
+       int i = 1;
+       int j = 0;
+       int p = 1;
+       while(i< arr.size() && j < dep.size())
+       {
+           if(arr[i] <= dep[j])
+           {
+               p++;
+               i++;
+           }
+           else {
+               p--;
+               j++;
+           }
+           ans = max(ans,p);
+       }
+        return ans;
     }
 };
