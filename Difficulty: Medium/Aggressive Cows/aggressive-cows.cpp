31fbcf31ed65082dd9427039c3169d3bb3e26ae7
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-12-08 18:06:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,45 +1,31 @@
 class Solution {
-  private:
-    int dis(int d,vector<int>&arr)
-    {
-        int c = 1;
-        int pre= arr[0];
-        for(int i=1;i<arr.size();i++)
-        {
-            if(arr[i] - pre >= d)
-            {
-                c++;
-                pre = arr[i];
-            }
-        }
-        return c;
-    }
-    
   public:
     int aggressiveCows(vector<int> &stalls, int k) {
         // code here
-        int n = stalls.size();
         sort(stalls.begin(),stalls.end());
         int l = 1;
-        int r = stalls[n-1]-stalls[0];
-        
-        int ans = 0;
-        while(l<=r)
-        {
-            int mid= l+(r-l)/2;
-            int c = dis(mid,stalls);
-            // cout<<"mid :"<<mid<<"cow:"<<c<<endl;
-            if(c >= k)
-            {
-                ans = mid;
-                l = mid+1;
+        int ans = INT_MAX;
+        int r = *max_element(stalls.begin(),stalls.end());
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            int c = 1;
+            int last = stalls[0];
+            int maxx = 0;
+            for(int i=1;i<stalls.size();i++){
+                if((last+mid) <= stalls[i]){
+                    maxx = max(maxx,stalls[i] - last);
+                    last = stalls[i];
+                    c++;
+                }
             }
-            else
-            {
-                r = mid-1;
+            if(c >= k){
+                ans = mid;
+                l = mid + 1;
+            }
+            else{
+                r = mid -1;
             }
         }
         return ans;
-        
     }
 };
