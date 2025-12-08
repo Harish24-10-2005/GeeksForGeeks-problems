6af747388fc286a244b4a803072d60d04da43dc0
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-12-08 18:28:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,39 +1,30 @@
 class Solution {
-  private:
-    int find(int pages,vector<int>&arr)
-    {
-        int st = 0;
-        int curr = 0;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(curr+arr[i] <= pages) curr+=arr[i];
-            else 
-            {
-                st++;
-                curr = arr[i];
-            }
-        }
-        return st;
-    }
   public:
     int findPages(vector<int> &arr, int k) {
         // code here
-        int n = arr.size();
-        if(k>n) return -1;
         int l = *max_element(arr.begin(),arr.end());
-        int r = accumulate(arr.begin(),arr.end(),0);
+        long long r = accumulate(arr.begin(), arr.end(), 0LL);
+
+        if (k > arr.size()) return -1;
         while(l<=r)
         {
             int mid = l + (r-l)/2;
-            int st= find(mid,arr);
-            // if(k == st) return mid;
-            if(st < k)
-            {
-                r = mid-1;
+            int stu = 0;
+            int p = 0;
+            for(int i :arr){
+                if(p+i > mid){
+                    stu++;
+                    p = i;
+                }
+                else{
+                    p+=i;
+                }
             }
-            else
-            {
-                l = mid+1;   
+            if(stu >= k){
+                l = mid + 1;
+            }
+            else{
+                r = mid -1;
             }
         }
         return l;
